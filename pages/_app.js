8c4dd1ce29cafb9e2ff8d92033a84a6f851a3da5
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2023-05-09 15:32:20 -0600

    SCSS Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,4 @@
-import './App.scss';
 import './index.scss';
-import './style/footer.scss';
-import './style/navbar.scss';
-import './style/product.scss';
-import './style/images.scss';
-import './style/info.scss';
 
 function App({Component, pageProps}) {
   return (
