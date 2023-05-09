349377aa15f65145e8ffb5abd23a634603d5593c
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2023-05-09 14:57:51 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/',
-          destination: '/index.html',
-        },
-      ]
-    },
+    distDir: '.next',
 }
+
+  
