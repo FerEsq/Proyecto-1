0134e0c9b5ee613bdfbe52160cd1938e79fa66fc
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2023-05-09 15:35:14 -0600

    Revert "SCSS Update"
    This reverts commit 8c4dd1ce29cafb9e2ff8d92033a84a6f851a3da5.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
+import './App.scss';
 import './index.scss';
+import './style/footer.scss';
+import './style/navbar.scss';
+import './style/product.scss';
+import './style/images.scss';
+import './style/info.scss';
 
 function App({Component, pageProps}) {
   return (
